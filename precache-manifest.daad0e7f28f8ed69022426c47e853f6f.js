bd1b330cd8804bf6aa95a116bb0337d5b4e79510
Nastia-rosy <nastiiarosy@gmail.com>
2020-04-27 09:38:55 +0300

    Updates

---
+++ precache-manifest.daad0e7f28f8ed69022426c47e853f6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51613e231d3e5cd4384f0cee72fbfaa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f2dbf8c9e6ef7b348e3a",
+    "url": "./static/css/main.0a887605.chunk.css"
+  },
+  {
+    "revision": "cdbfa4b0ed9a17660ba3",
+    "url": "./static/js/2.b0f90b1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b0f90b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2dbf8c9e6ef7b348e3a",
+    "url": "./static/js/main.761a55bc.chunk.js"
+  },
+  {
+    "revision": "dc13ecc3e0fecfe0c39b",
+    "url": "./static/js/runtime-main.33c19533.js"
+  }
+]);
